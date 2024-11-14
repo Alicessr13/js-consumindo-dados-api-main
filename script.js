d44fbe23b847e283f00817dca89f06d818192d89
Alice <alicessr12@gmail.com>
2024-11-14 13:17:21 -0300

    update script.js

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
     })
     .catch(erro => console.log(erro))
     .finally(mensagem => console.log('Processamento concluído'));
+//promise = promesa de que algo vai acontecer, pode retornar resolvida ou rejeitada
 //fetch = metodo assincrono que recebe a url da api, then = então, pega a resposta e faz algo, só faz se a promessa for resolvida, 
 //catch = pegue, captura o erro que acontecer
-//promise = promesa de que algo vai acontecer, pode retornar resolvida ou rejeitada
+//finally = finalmente, independente da resposta da promessa ele vai retornar o finally
 
 console.log(consultaCEP);
